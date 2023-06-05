21de74388d372999238938cdafb98c382585fb6d
MrRamPoudel <rp164@uakron.edu>
2023-06-04 21:19:10 -0400

    Add membership info

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
   <div>
  <Nav />
- <div className ="w-auto h-fit relative"> <Image src={gymgirl} alt="Picture of girl"  /> </div>
+ <div className ="w-auto h-fit relative"> <Image src={gymgirl} alt="Picture of girl" className="hidden sm:block"  /> </div>
  </div>
  );
 }
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,5 +1,5 @@
 import Nav from "@/components/Nav"
-//import PricingPage from "@/components/Price"
+import PricingPage from "@/components/Price"
 import Membership from "@/components/Membership"
 export default function Pricing(){
     return (<div>
