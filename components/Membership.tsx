931e1625a071cf46fa43ff68fe3e6ade1ba8b00f
MrRamPoudel <rp164@uakron.edu>
2023-06-04 21:44:58 -0400

    Fix lettering

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -13,7 +13,7 @@ const Membership: React.FC = () => {
                 </div>
                 <div className='pt-24 flex flex-row '>
                     <div className='relative w-80 p-8 pr-16 bg-red-300 text-center rounded-3xl '>
-                        <p className='text-md text-black'>Basic</p>
+                        <p className='text-md text-black font-semibold'>Basic</p>
                         <p className='pt-2'>
                             <span className="text-black align-top">$ </span>
                             <span className="text-3xl font-semibold">10</span>
@@ -41,14 +41,16 @@ const Membership: React.FC = () => {
                             <p className='text-white font-semibold'>Everything in Basic plan</p>
                             <p className='text-white font-semibold'>Access to all 325 clubs</p>
                             <p className='text-white font-semibold'>Hydro massage</p>
-                            <p className='text-white font-semibold'>25 % Discounts on Select<small>*</small>merchandise and supplements.</p>
+                            <span className='text-white font-semibold text-sm'>25% Discounts on Select</span>
+                            <span className ='text-white font-semibold text-xsm'>*</span> 
+                            <span className=" text-white font-semibold text-sm"> merchandise and supplements</span>
                         </div>
                         <div className='rounded-3xl relative pt-4'>
                             <button className="bottom-0 w-4/5 bg-blue-700 text-xl text-white px-4 rounded-3xl">JOIN</button>
                         </div>
                     </div>
                     <div className='w-80 p-8 pr-8 bg-blue-300 text-center rounded-3xl '>
-                        <p className='text-md text-black'>Elite</p>
+                        <p className='text-md text-black font-semibold'>Elite</p>
                         <p className='pt-2'>
                             <span className="text-gray-400 align-top">$ </span>
                             <span className="text-3xl font-semibold">34</span>
@@ -58,16 +60,17 @@ const Membership: React.FC = () => {
                             <p className='text-white font-semibold'>Everything in Pro plan</p>
                             <p className='text-white font-semibold'>Access to all 350 clubs</p>
                             <p className='text-white font-semibold'>Access to Premium gym equipment</p>
-                            <p className='text-white font-semibold'>50% Discount on Select <small>*</small> merchandise and supplements</p>
-                            <p className='text-white font-semibold'>Guest passes</p>
+                            <span className='text-white font-semibold text-sm'>50% Discounts on Select</span>
+                            <span className ='text-white font-semibold text-xsm'>*</span> 
+                            <span className=" text-white font-semibold text-sm"> merchandise and supplements</span>                            <p className='text-white font-semibold'>Guest passes</p>
                         </div>
                         <div className='rounded-3xl relative pt-4 '>
                             <button className="bottom-0 w-4/5 bg-blue-700 text-xl text-white px-4 rounded-3xl">JOIN</button>
                         </div>
                     </div>
-
                 </div>
             </div>
-        </div>)
+        </div>
+        )
 }
 export default Membership;
